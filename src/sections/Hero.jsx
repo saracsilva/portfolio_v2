e8328459e6df89727527d78055ed26df22630e4d
saracsilva <saracarolinasilva@gmail.com>
2026-02-18 14:08:54 +0100

    Remove unused components and styles and add Hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <div className="flex flex-col items-center gap-8 font-serif">
+      <h1 className="text-9xl tracking-widest ">Sara Silva</h1>
+      <h2 className="text-5xl italic">Brazilian</h2>
+      <div className="text-4xl">
+        <h2>
+          I’m a{" "}
+          <span className="font-mono">
+            FrontEnd Developer
+            <span className="text-primary">_</span>
+          </span>
+        </h2>
+        <h2 className="text-4xl tracking-[.13em]">Based in Enschede, NL.</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
