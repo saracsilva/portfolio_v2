1b2b5e6e779393745d82d38b66f4778ca3ed35cd
saracsilva <saracarolinasilva@gmail.com>
2026-02-19 10:24:46 +0100

    Add Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./sections/Hero";
 import Navbar from "./layout/Navbar";
+import Projects from "./sections/Projects";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Projects />
     </>
   );
 }
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../components/SectionTitle";
+
+function Projects() {
+  return (
+    <section id="projects">
+      <SectionTitle title="Projects" />
+      <p>Here are some of the projects I've worked on.</p>
+    </section>
+  );
+}
+
+export default Projects;
