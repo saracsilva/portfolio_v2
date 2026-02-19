37ec4ad08f0729d5ed81dbc0ca186ccb700ee6f6
saracsilva <saracarolinasilva@gmail.com>
2026-02-19 10:03:04 +0100

    Add container class for layout consistency
    Added to SectionTitle and Navbar to improve layout

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,9 +1,11 @@
 function SectionTitle({ title }) {
   return (
-    <h2 className="section-title border-t-2 border-b p-4 text-3xl font-mono">
-      {title}
-      <span className="text-primary">_</span>
-    </h2>
+    <div className="border-t-2 border-b">
+      <h2 className="section-title container p-4 text-3xl font-mono">
+        {title}
+        <span className="text-primary">_</span>
+      </h2>
+    </div>
   );
 }
 
