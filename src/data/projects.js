ca2049de744509fd396f327cc689e6d1c091ec9e
saracsilva <saracarolinasilva@gmail.com>
2026-02-21 14:57:05 +0100

    Add projects data file and display cards from it
    Also add more images to the projects section

---
+++ src/data/projects.js
@@ -0,0 +1,46 @@
+import project1Img from "/projects/project_01.png";
+import project2Img from "/projects/project_02.png";
+import project3Img from "/projects/project_03.png";
+import project4Img from "/projects/project_04.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "My Dictionary",
+    image: project1Img,
+    description:
+      "This is a personal project, an English Dictionary React app using an API.",
+    github: "https://github.com/saracsilva/dictionary",
+    live: "https://glowing-stardust-4eab74.netlify.app/search",
+  },
+  {
+    id: 2,
+    title: "UpManager",
+    image: project2Img,
+    description:
+      "This projects was executed during my Ironhack Bootcamp. It's the final project and I did it together with my colegues Shirin and Stephanie. We decided to make a management tool to help to organize projects and events from a company. We used MongoDB for the Database, Express for the BackEnd and React in the FronEnd. To give a better look to the APP, we used Material UI and Mantine.",
+    github: {
+      frontend: "https://github.com/stephahos/upmanager",
+      backend: "https://github.com/stephahos/up_manager_api",
+    },
+    live: "https://precious-starship-8d8098.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Could be pizza",
+    image: project3Img,
+    description:
+      "This was the second project. I made it together with my colleague Andrea. We decided to make an App where you can put all kinds of crazy pizza flavors on it, so everybody that visits the app can know the different types of pizza that exist in the world. We used MongoDB for the Database, Express for the BackEnd and JavaScript and EJS in the FrontEnd. To give a better look to the APP, we used CSS and a bit of Bootstrap.",
+    github: "https://github.com/andreiacpafonso/couldbepizza",
+    live: "https://lilac-drill-cuff.cyclic.app/",
+  },
+  {
+    id: 4,
+    title: "Dinocorn Game",
+    image: project4Img,
+    description:
+      "This was my first project. It is a game like the original dinosaur one from Chrome. I've decided to give a happier look to the game to make it more enjoyable. It's all made with JavaScript, CSS and HTML.",
+    github: "https://github.com/saracsilva/the-dinocorn-game",
+    live: "https://saracsilva.github.io/the-dinocorn-game/",
+  },
+];
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,17 +1,21 @@
 import SectionTitle from "../components/SectionTitle";
+import { projects } from "../data/projects";
 import Card from "../components/Card";
 
 function Projects() {
   return (
     <section id="projects">
       <SectionTitle title="Projects" />
-      <div className="container my-28 px-16">
-        <div className="grid grid-cols-4 gap-8">
-          <Card title="Upmanager" image="/projects/project_02.png" />
-          <Card title="Upmanager" image="/projects/project_02.png" />
-          <Card title="Upmanager" image="/projects/project_02.png" />
-          <Card title="Upmanager" image="/projects/project_02.png" />
-        </div>
+      <div className="container my-28 px-8 md:px-16">
+        <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8">
+          {projects.map((project) => (
+            <Card
+              key={project.id}
+              title={project.title}
+              image={project.image}
+            />
+          ))}
+        </ul>
       </div>
     </section>
   );
