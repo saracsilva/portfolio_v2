233c5a21985f8e986733df77fe2f4599d325dc59
saracsilva <saracarolinasilva@gmail.com>
2026-02-23 21:07:05 +0100

    add technologies list

--- src/sections/Technologies.jsx
+++ src/sections/Technologies.jsx
@@ -4,6 +4,32 @@ function Technologies() {
   return (
     <section id="technologies">
       <SectionTitle title="Technologies" />
+      <div className="container my-20 px-8 md:px-16 border-y border-text-color">
+        <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 font-mono font-medium">
+          <li>
+            <ul className="flex flex-col items-center border-r py-4">
+              <li>JavaScript</li>
+              <li>Ruby on Rails</li>
+              <li>HTML5</li>
+              <li>CSS</li>
+            </ul>
+          </li>
+
+          <li>
+            <ul className="flex flex-col items-center border-r py-4 h-full">
+              <li>React</li>
+              <li>Tailwind</li>
+            </ul>
+          </li>
+
+          <li>
+            <ul className="flex flex-col items-center py-4">
+              <li>Git / GitHub</li>
+              <li>Figma</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
